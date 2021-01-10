81b50dce82fe92f616ec07fba5ff575d2916ed4b
Velo <hartley.max@outlook.com>
2021-01-10 22:50:05 +0000

    nav styling restored

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import { pages } from '../infoJs/pages';
 import useWindowSize from '../hooks/useWindowSize';
+import style from '../styles/nav.module.css';
 
 export default function Nav(props) {
 	const [width] = useWindowSize();
@@ -16,22 +17,29 @@ export default function Nav(props) {
 		setShowNav(width < 1050 ? false : true);
 	}, [width]);
 
+	const outerLineStyle = { transform: showNav ? 'none' : 'rotate3d(1, 0.5, 1, 360deg)' };
+	const middleLineStyle = { transform: showNav ? 'none' : 'rotate3d(1, 0.5, 1, -360deg)' };
+
+	// className={`line ${showNav ? '' : 'line-animated'}`}
 	return (
-		<nav>
-			<div className='overflow-container'>
-				<Link href='/' className='logo'>
-					<a>
+		<nav className={style.nav}>
+			<div className={style.overflowContainer}>
+				<Link href='/'>
+					<a className={style.logo}>
 						<img src='../images/green-lawns-logo.png' alt='logo placeholder.' />
 						<h4>Green Lawns {width > 1050 && <br></br>}Boarding Kennels</h4>
 					</a>
 				</Link>
 			</div>
-			<div className='nav-button' onClick={handleNavButtonClick}>
-				<div className={`line ${showNav ? '' : 'line-animated'}`}></div>
-				<div className={`line ${showNav ? '' : 'middle-line-animated'}`}></div>
-				<div className={`line ${showNav ? '' : 'line-animated'}`}></div>
+			<div className={style.navButton} onClick={handleNavButtonClick}>
+				<div className={style.line} style={outerLineStyle}></div>
+				<div className={style.line} style={middleLineStyle}></div>
+				<div className={style.line} style={outerLineStyle}></div>
 			</div>
-			<ul style={{ pointerEvents: width > 1050 ? 'auto' : !showNav ? 'none' : 'auto' }}>
+			<ul
+				className={style.links}
+				style={{ pointerEvents: width > 1050 ? 'auto' : !showNav ? 'none' : 'auto' }}
+			>
 				{pages.map((page, i) => (
 					<li
 						key={`${i}${page}`}
