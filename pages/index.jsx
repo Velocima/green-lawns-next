460f547a2f8096fb21f18c25b773e28cf507de60
Velo <hartley.max@outlook.com>
2021-01-11 00:04:45 +0000

    index page component styling restored

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import About from '../components/about';
 import Nav from '../components/nav';
 import Footer from '../components/footer';
+import style from '../styles/index.module.css';
 
 export default function Home() {
 	return (
@@ -12,18 +13,20 @@ export default function Home() {
 				<link rel='icon' href='/images/green-lawns-logo.png' />
 			</Head>
 			<Nav />
-			<main>
-				<div className='title-banner'>
-					<div className='titles'>
-						<h1>Green Lawns Boarding Kennels</h1>
-						<h2>Your dog's home away from home</h2>
+			<main className={style.main}>
+				<section className={style.landing}>
+					<div className={style.banner}>
+						<div className={style.titles}>
+							<h1>Green Lawns Boarding Kennels</h1>
+							<h2>Your dog's home away from home</h2>
+						</div>
 					</div>
-				</div>
-				<div className='call-to-action'>
-					<Link href='/contact'>
-						<a>Book Now</a>
-					</Link>
-				</div>
+					<div className={style.callToAction}>
+						<Link href='/contact'>
+							<a>Book Now</a>
+						</Link>
+					</div>
+				</section>
 				<About />
 			</main>
 			<Footer />
