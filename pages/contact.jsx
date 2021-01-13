bee460ee47d4d44da371972de70e827ec5d56f81
Velo <hartley.max@outlook.com>
2021-01-13 02:45:19 +0000

    basic styling completed for contact page£

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -6,37 +6,6 @@ import Footer from '../components/footer';
 import style from '../styles/contact.module.css';
 
 export default function Contact() {
-	const [width, height] = useWindowSize();
-	const [animationIsPlaying, setAnimationIsPlaying] = useState({
-		openingHours: 'paused',
-		address: 'paused',
-	});
-
-	const openingHoursRef = useRef(null),
-		addressRef = useRef(null);
-
-	useEffect(() => {
-		const topPosition = (element) => element.current.getBoundingClientRect().top;
-
-		const openingHoursPosition = topPosition(openingHoursRef),
-			addressPosition = topPosition(addressRef);
-
-		const onScroll = () => {
-			const scrollPosition = window.scrollY + height;
-			if (
-				animationIsPlaying.openingHours !== 'running' &&
-				openingHoursPosition < scrollPosition
-			) {
-				setAnimationIsPlaying((prevState) => ({ ...prevState, openingHours: 'running' }));
-			}
-			if (animationIsPlaying.address !== 'running' && addressPosition < scrollPosition) {
-				setAnimationIsPlaying((prevState) => ({ ...prevState, address: 'running' }));
-			}
-		};
-		window.addEventListener('scroll', onScroll);
-		return () => window.removeEventListener('scroll', onScroll);
-	}, [width, height]);
-
 	return (
 		<>
 			<Head>
@@ -45,70 +14,83 @@ export default function Contact() {
 			</Head>
 			<Nav />
 			<main className={style.contact}>
+				<div className={style.banner}></div>
 				<section className={style.contactInfo}>
-					<div className={style.banner}>
-						<h1>Get In Touch</h1>
-						<h2>We'd Love to Hear from You!</h2>
+					<div className=''>
+						<h1>BOOK NOW:</h1>
+						<a href='tel:01600740111'>
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								height='24'
+								viewBox='0 0 24 24'
+								width='24'
+							>
+								<path d='M0 0h24v24H0z' fill='none' />
+								<path d='M20.01 15.38c-1.23 0-2.42-.2-3.53-.56-.35-.12-.74-.03-1.01.24l-1.57 1.97c-2.83-1.35-5.48-3.9-6.89-6.83l1.95-1.66c.27-.28.35-.67.24-1.02-.37-1.11-.56-2.3-.56-3.53 0-.54-.45-.99-.99-.99H4.19C3.65 3 3 3.24 3 3.99 3 13.28 10.73 21 20.01 21c.71 0 .99-.63.99-1.18v-3.45c0-.54-.45-.99-.99-.99z' />
+							</svg>
+							01600 740111
+						</a>
+						<a href='tel:07815534990'>
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								height='24'
+								viewBox='0 0 24 24'
+								width='24'
+							>
+								<path d='M0 0h24v24H0z' fill='none' />
+								<path d='M17 1.01L7 1c-1.1 0-2 .9-2 2v18c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V3c0-1.1-.9-1.99-2-1.99zM17 19H7V5h10v14z' />
+							</svg>
+							07815 534990
+						</a>
+						<a href='mailto:greenlawnskennels@icloud.com'>
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								height='24'
+								viewBox='0 0 24 24'
+								width='24'
+							>
+								<path d='M0 0h24v24H0z' fill='none' />
+								<path d='M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z' />
+							</svg>
+							greenlawnskennels@icloud.com
+						</a>
 					</div>
-					<div className={style.phoneNumbers}>
-						<p>
-							<span className={style.phoneNumbersHeader}>Book now via:</span>
-							Telephone:{' '}
-							<a href='tel:01600740111'>
-								<span>01600 740111</span>
-							</a>{' '}
-							<br />
-							Mobile:{' '}
-							<a href='tel:07815534990'>
-								<span>07815 534990</span>
-							</a>
-							<br />
-							Email:{' '}
-							<a href='mailto:greenlawnskennels@icloud.com'>
-								<span className={style.email}>greenlawnskennels@icloud.com</span>
-							</a>
-						</p>
+				</section>
+				<iframe
+					src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2468.211355908111!2d-2.7741205!3d51.7840232!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4871c7a3c31bcd37%3A0x2e3a5debcce221b8!2sGreen%20Lawns%20Boarding%20Kennels!5e0!3m2!1sen!2suk!4v1604681217217!5m2!1sen!2suk'
+					width='600'
+					height='450'
+					frameBorder='0'
+					allowFullScreen=''
+					aria-hidden='false'
+					tabIndex='0'
+					title='Google maps'
+				></iframe>
+				<section className={style.tables}>
+					<div className={style.table}>
+						<table>
+							<tbody>
+								<tr>
+									<th colSpan='2'>OPENING HOURS</th>
+								</tr>
+								<tr>
+									<td>Monday - Friday</td>
+									<td>
+										9:00am - 11:00am
+										<br />
+										2:00pm - 4:00pm
+									</td>
+								</tr>
+								<tr>
+									<td>Saturday - Sunday</td>
+									<td>9:00am - 11:00am</td>
+								</tr>
+							</tbody>
+						</table>
 					</div>
-					<iframe
-						src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2468.211355908111!2d-2.7741205!3d51.7840232!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4871c7a3c31bcd37%3A0x2e3a5debcce221b8!2sGreen%20Lawns%20Boarding%20Kennels!5e0!3m2!1sen!2suk!4v1604681217217!5m2!1sen!2suk'
-						width='600'
-						height='450'
-						frameBorder='0'
-						allowFullScreen=''
-						aria-hidden='false'
-						tabIndex='0'
-						title='Google maps'
-					></iframe>
-					<div className={style.tableBackground}></div>
-					<table
-						ref={openingHoursRef}
-						style={{ animationPlayState: animationIsPlaying.openingHours }}
-					>
-						<tbody>
-							<tr>
-								<th colSpan='2'>Opening Hours</th>
-							</tr>
-							<tr>
-								<td>Monday - Friday</td>
-								<td>
-									9:00am - 11:00am
-									<br></br>
-									2:00pm - 4:00pm
-								</td>
-							</tr>
-							<tr>
-								<td>Saturday - Sunday</td>
-								<td>9:00am - 11:00am</td>
-							</tr>
-						</tbody>
-					</table>
 					<div className={style.address}>
-						<p
-							ref={addressRef}
-							style={{ animationPlayState: animationIsPlaying.address }}
-						>
-							<span className={style.addressHeader}>Our Address</span>
-							<br />
+						<h2>OUT ADDRESS</h2>
+						<p>
 							Green Lawns
 							<br />
 							Mitchel Troy
