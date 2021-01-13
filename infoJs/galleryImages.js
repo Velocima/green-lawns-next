df780ba7591c60c59ad062dc70d7af528ac466f8
Velo <hartley.max@outlook.com>
2021-01-13 12:47:00 +0000

    image alts for the gallery populated

--- infoJs/galleryImages.js
+++ infoJs/galleryImages.js
@@ -1,17 +1,51 @@
 export const galleryImages = [
-	{ image: '/images/gallery/runnig.jpg', alt: '' },
-	{ image: '/images/gallery/also-running.jpg', alt: '' },
-	{ image: '/images/gallery/good-boy-ball.jpg', alt: '' },
-	{ image: '/images/gallery/good-boy-three.jpg', alt: '' },
-	{ image: '/images/gallery/good-boy-two.jpg', alt: '' },
-	{ image: '/images/gallery/hope-and-ted.jpg', alt: '' },
-	{ image: '/images/gallery/husky.jpg', alt: '' },
-	{ image: '/images/gallery/nani.jpg', alt: '' },
-	{ image: '/images/gallery/pair-of-good-boys.jpg', alt: '' },
-	{ image: './images/gallery/he-sits.jpg', alt: '' },
-	{ image: './images/gallery/stick.jpg', alt: '' },
-	{ image: './images/gallery/ted-1.jpg', alt: '' },
-	{ image: './images/gallery/thor.jpg', alt: '' },
-	{ image: './images/gallery/trio.jpg', alt: '' },
-	{ image: './images/gallery/barley.jpg', alt: '' },
+	{ image: '/images/gallery/runnig.jpg', alt: 'An Irish Setter puppy running around.' },
+	{ image: '/images/gallery/also-running.jpg', alt: 'Two happy dogs running in our field.' },
+	{
+		image: '/images/gallery/good-boy-ball.jpg',
+		alt: 'Nick the Collie waiting to play with a football.',
+	},
+	{
+		image: '/images/gallery/good-boy-three.jpg',
+		alt: 'Another happy guest dog smiling at the camera.',
+	},
+	{
+		image: '/images/gallery/good-boy-two.jpg',
+		alt: 'A black and white collie on its walk around our field.',
+	},
+	{
+		image: '/images/gallery/hope-and-ted.jpg',
+		alt: 'One of our resident dogs, Ted out for a walk in the hills.',
+	},
+	{ image: '/images/gallery/husky.jpg', alt: 'An elder Husky walking across our field' },
+	{
+		image: '/images/gallery/nani.jpg',
+		alt: 'Nick the Collie on a walk. One of our regular guests.',
+	},
+	{
+		image: '/images/gallery/pair-of-good-boys.jpg',
+		alt: 'Two floofers. A pair of wonderful good boys.',
+	},
+	{
+		image: './images/gallery/he-sits.jpg',
+		alt: 'A long haired daschund having a lie down on its bed.',
+	},
+	{
+		image: './images/gallery/stick.jpg',
+		alt: 'Titan, one of our resident Black Laboradors showing off his stick.',
+	},
+	{
+		image: './images/gallery/ted-1.jpg',
+		alt: 'Ted the Sproker Spaniel on a special walk for his first birthday.',
+	},
+	{
+		image: './images/gallery/thor.jpg',
+		alt: 'Titan one of our resident Black Laboradors out for a walk in the woods.',
+	},
+	{
+		image: './images/gallery/trio.jpg',
+		alt:
+			'Titan, Zues and Ted, our three resident dogs looking up to Julie, the business owner.',
+	},
+	{ image: './images/gallery/barley.jpg', alt: 'A Golden Retriever having fun in the snow.' },
 ];
