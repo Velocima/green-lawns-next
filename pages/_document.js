6585b876b7b5e93868da34545ee02883f5512955
Velo <hartley.max@outlook.com>
2021-01-22 14:54:34 +0000

    google tag manager tag added!

--- pages/_document.js
+++ pages/_document.js
@@ -12,15 +12,21 @@ class MyDocument extends Document {
 				<Head />
 				{/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
 				<script
-					async
-					src='https://www.googletagmanager.com/gtag/js?id=G-1KMZ8YLRKX'
-				></script>
-				<script>
-					window.dataLayer = window.dataLayer || []; function gtag()
-					{dataLayer.push(arguments)}
-					gtag('js', new Date()); gtag('config', 'G-1KMZ8YLRKX');
-				</script>
+					dangerouslySetInnerHTML={{
+						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','G-1KMZ8YLRKX');`,
+					}}
+				/>
 				<body>
+					<noscript
+						dangerouslySetInnerHTML={{
+							__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=G-1KMZ8YLRKX"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+						}}
+					/>
 					<Main />
 					<NextScript />
 				</body>
